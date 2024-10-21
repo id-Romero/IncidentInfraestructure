05e41fd39c0b7f917224a430ca799ef0f5d0a03e
Diego Romero <irommero@protonmail.com>
2024-10-20 22:46:56 -0600

    puch

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -114,7 +114,7 @@ resource "azurerm_linux_virtual_machine" "IN_VM" {
 
   admin_ssh_key {
     username   = "${var.admin_username}"
-    public_key = file("./keys/711incident_server.pub")
+    public_key = file("../../env/dev/keys/keys.pub")
   }
 
   provisioner "file" {
@@ -125,7 +125,7 @@ resource "azurerm_linux_virtual_machine" "IN_VM" {
   connection {
     type        = "ssh"
     user        = "${var.admin_username}"
-    private_key = file("./keys/711incident_server")
+    private_key = file("../../env/dev/keys/keys")
     host        = self.public_ip_address
   }
 
