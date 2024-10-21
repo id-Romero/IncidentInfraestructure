350f98c92cf4dde3549dc6abfef09aaef667e421
Diego Romero <irommero@protonmail.com>
2024-10-20 22:24:20 -0600

    puch

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -2,7 +2,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name = "NetworkWatcherRG"
-    storage_account_name = "711incidentdiego"
+    storage_account_name = "711incidentcarlin"
     container_name = "711-74842-tfstate"
     key = "terraform.tfstate"
   }
